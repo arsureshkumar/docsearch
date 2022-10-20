74d8d82f68ec387ac860c36a21edd1cbf930ce43
arsureshkumar <arsureshkumar@ucsd.edu>
2022-10-20 11:07:59 -0700

    scripts

--- DocSearchTest.java
+++ DocSearchTest.java
@@ -1,9 +1,13 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import org.junit.Test;
-import org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertEquals;
 
 public class DocSearchTest{
   @Test
-  public void emptyTest(){
-    assertEquals()
+  public void emptyTest() throws IOException, URISyntaxException{
+    Handler handler = new Handler("technical");
+    assertEquals("Don't know how to handle that path!", handler.handleRequest(new URI("")));
   }
 }
